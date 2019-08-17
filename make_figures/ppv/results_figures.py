e3cc3fe6a6ffdafc4aadd0d0289ee5ce7227d26f
AntoinePassemiers <apassemi@ulb.ac.be>
2019-08-17 16:15:05 +0200

    New results

--- make_figures/ppv/results_figures.py
+++ make_figures/ppv/results_figures.py
@@ -13,22 +13,24 @@ def load_results(filepath):
     data = { col_name: list() for col_name in col_names }
     for line in lines[1:]:
         el = line.replace('\n', '').replace(' ', '').split(',')
-        if len(el) > 2:
+        if len(el) > 6:
             for i in range(len(el)):
                 if i > 0:
                     el[i] = float(el[i])
-                print(len(col_names), len(el))
                 data[col_names[i]].append(el[i])
     return data
 
 
 casp11 = load_results('casp11.full.txt')
 cameo = load_results('cameo.full.txt')
+membrane = load_results('membrane.full.txt')
+
 casp11_plmdca = load_results('casp11.plmdca.txt')
-casp11_new = load_results('casp11.new.txt')
+cameo_plmdca = load_results('cameo.plmdca.txt')
+membrane_plmdca = load_results('membrane.plmdca.txt')
 
 
-for name, results in zip(['CASP11', 'CAMEO'], [casp11, casp11_new]):
+for name, results in zip(['CASP11', 'CAMEO', 'Membrane'], [casp11, cameo, membrane]):
     print('%s PPV: %f | short-range: %f | Medium-range: %f | Long-range: %f' % (name, np.mean(results['PPV']), np.mean(results['PPV-short']), np.mean(results['PPV-medium']), np.mean(results['PPV-long'])))
     print('%s PPV/2: %f | short-range: %f | Medium-range: %f | Long-range: %f' % (name, np.mean(results['PPV/2']), np.mean(results['PPV/2-short']), np.mean(results['PPV/2-medium']), np.mean(results['PPV/2-long'])))
     print('%s PPV/5: %f | short-range: %f | Medium-range: %f | Long-range: %f' % (name, np.mean(results['PPV/5']), np.mean(results['PPV/5-short']), np.mean(results['PPV/5-medium']), np.mean(results['PPV/5-long'])))
@@ -36,33 +38,35 @@ for name, results in zip(['CASP11', 'CAMEO'], [casp11, casp11_new]):
     print('')
 
 
+results = membrane
+results_dca = membrane_plmdca
+
 f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-ax2.set_axis_off()
-options = { 'marker': 'x', 's': 25 }
+options = { 'marker': 'o', 's': 25 }
 for ax, metric, name in zip(
-        [ax1,ax3,ax4],
-        ['PPV/5','PPV/5-medium','PPV/5-long'],
-        ['All contacts','Medium-range contacts','Long-range contacts']):
-    xs, ys = np.log(casp11_plmdca['Meff']), casp11_plmdca[metric]
+        [ax1,ax2,ax3,ax4],
+        ['PPV/5','PPV/5-long', 'PPV/5-medium','PPV/5-short'],
+        ['All contacts','Long-range contacts','Medium-range contacts','Short-range contacts']):
+    xs, ys = np.log(results_dca['Meff']), results_dca[metric]
     b, m = polyfit(xs, ys, 1)
     ax.scatter(xs, ys, label='plmDCA', color=colors[0], **options)
     xs = np.asarray([np.min(xs), np.max(xs)])
     ax.plot(xs, b + m * xs, '--', color=colors[0])
-    xs, ys = np.log(casp11['Meff']), casp11[metric]
+    xs, ys = np.log(results['Meff']), results[metric]
     b, m = polyfit(xs, ys, 1)
-    ax.scatter(xs, ys, label='Wynona', color=colors[5], **options)
+    ax.scatter(xs, ys, label='Proposed method', color=colors[5], **options)
     xs = np.asarray([np.min(xs), np.max(xs)])
     ax.plot(xs, b + m * xs, '--', color=colors[5])
     ax.set_ylabel('Best-L/5 PPV', fontsize=12)
     if ax in [ax3, ax4]:
         ax.set_xlabel('ln(Meff)', fontsize=12)
-    ax.set_ylim(-.05, np.max(ys))
+    ax.set_ylim(-.05, np.max(ys) + .05)
     ax.set_title(name, fontsize=12)
     ax.legend(prop={ 'size': 12 })
 plt.show()
 
-# import sys; sys.exit(0)
 
+"""
 options = { 'marker': 'o', 's': 18 }
 scatter_axes = plt.subplot2grid(
         (3, 3), (1, 0), rowspan=2, colspan=2)
@@ -82,3 +86,4 @@ x_hist_axes.hist(cameo['RMSD'], color=colors[2], bins=20, orientation='vertical'
 y_hist_axes.hist(cameo['TM-score'], color=colors[2], bins=20, orientation='horizontal')
 y_hist_axes.axhline(y=.20, linestyle='--', color='grey')
 plt.show()
+"""
